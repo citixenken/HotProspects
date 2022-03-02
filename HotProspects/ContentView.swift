e02fc3100c8329776d535590f2caa5bc3407695f
citixenken <kmuyesu@gmail.com>
2022-03-02 14:57:44 +0300

    creating tabs with tabview and tabitem

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -8,9 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var selectedTab = "Print"
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView(selection: $selectedTab) {
+            Text("Tab One")
+                .onTapGesture {
+                    selectedTab = "Display"
+                }
+                .tabItem {
+                    Label("Print", systemImage: "printer")
+                }
+                .tag("Print")
+            Text("Tab Two")
+                .tabItem{
+                    Label("Display", systemImage: "display")
+                }
+                .tag("Display")
+            Text("Tab Three")
+                .tabItem{
+                    Label("Scan", systemImage: "scanner")
+                }
+                .tag("Scan")
+            Text("Tab Four")
+                .tabItem{
+                    Label("Server Rack", systemImage: "server.rack")
+                }
+                .tag("Server Rack")
+        }
     }
 }
 
