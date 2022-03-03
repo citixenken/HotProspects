c46a2863e3d585138ee97447f4b570f28bc952b2
citixenken <kmuyesu@gmail.com>
2022-03-03 08:10:57 +0300

    adding swipe action buttons

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -13,73 +13,91 @@ struct ContentView: View {
     
     var body: some View {
         
-        VStack {
-            Text("Hello, CK!")
-                .padding()
-                .background(backgroundColor)
-            
-            Text("Change Color")
-                .padding()
-                .contextMenu {
-                    Button(role: .destructive){
-                        backgroundColor = .red
-                    } label: {
-                        Label("Red", systemImage: "checkmark.circle.fill")
-                            .foregroundColor(.red)
-                    }
-                    
+        List {
+            Text("Baby Keem")
+                .swipeActions {
                     Button {
-                        backgroundColor = .green
+                        print("Hi!")
                     } label: {
-                        Label("Green", systemImage: "checkmark.circle.fill")
-                            .foregroundColor(.green)
+                        Label("Send message", systemImage: "message")
                     }
-
+                }
+                .swipeActions(edge: .leading) {
                     Button {
-                        backgroundColor = .blue
+                        print("Hey!")
                     } label: {
-                        Label("Blue", systemImage: "checkmark.circle.fill")
-                            .foregroundColor(.blue)
+                        Label("Pin", systemImage: "pin")
                     }
-
-
+                    .tint(.mint)
                 }
         }
-//        Image("example")
-//            .interpolation(.none)
-//            .resizable()
-//            .scaledToFit()
-//            .frame(maxHeight: .infinity)
-//            .background(.black)
-//            .ignoresSafeArea()
+        //        VStack {
+        //            Text("Hello, CK!")
+        //                .padding()
+        //                .background(backgroundColor)
+        //
+        //            Text("Change Color")
+        //                .padding()
+        //                .contextMenu {
+        //                    Button(role: .destructive){
+        //                        backgroundColor = .red
+        //                    } label: {
+        //                        Label("Red", systemImage: "checkmark.circle.fill")
+        //                            .foregroundColor(.red)
+        //                    }
+        //
+        //                    Button {
+        //                        backgroundColor = .green
+        //                    } label: {
+        //                        Label("Green", systemImage: "checkmark.circle.fill")
+        //                            .foregroundColor(.green)
+        //                    }
+        //
+        //                    Button {
+        //                        backgroundColor = .blue
+        //                    } label: {
+        //                        Label("Blue", systemImage: "checkmark.circle.fill")
+        //                            .foregroundColor(.blue)
+        //                    }
+        //
+        //
+        //                }
+        //        }
+        //        Image("example")
+        //            .interpolation(.none)
+        //            .resizable()
+        //            .scaledToFit()
+        //            .frame(maxHeight: .infinity)
+        //            .background(.black)
+        //            .ignoresSafeArea()
     }
     
-//    @State private var output = ""
-//
-//    var body: some View {
-//        Text(output)
-//            .task {
-//                await fetchReadings()
-//            }
-//}
+    //    @State private var output = ""
+    //
+    //    var body: some View {
+    //        Text(output)
+    //            .task {
+    //                await fetchReadings()
+    //            }
+    //}
     
-//    func fetchReadings() async {
-////        do {
-////            let url = URL(string: "https://hws.dev/readings.json")!
-////            let (data, _) = try await URLSession.shared.data(from: url)
-////            let readings = try JSONDecoder().decode([Double].self, from: data)
-////            output = "Found \(readings.count) readings"
-////        } catch {
-////            print("Download error")
-////        }
-//
-//        let fetchTask = Task { () -> String in
-//            let url = URL(string: "https://dev/readings.json")!
-//            let (data, _) = try await URLSession.shared.data(from: url)
-//            let readings = try JSONDecoder().decode([Double].self, from: data)
-//            return "Found \(readings.count) readings"
-//        }
-//    }
+    //    func fetchReadings() async {
+    ////        do {
+    ////            let url = URL(string: "https://hws.dev/readings.json")!
+    ////            let (data, _) = try await URLSession.shared.data(from: url)
+    ////            let readings = try JSONDecoder().decode([Double].self, from: data)
+    ////            output = "Found \(readings.count) readings"
+    ////        } catch {
+    ////            print("Download error")
+    ////        }
+    //
+    //        let fetchTask = Task { () -> String in
+    //            let url = URL(string: "https://dev/readings.json")!
+    //            let (data, _) = try await URLSession.shared.data(from: url)
+    //            let readings = try JSONDecoder().decode([Double].self, from: data)
+    //            return "Found \(readings.count) readings"
+    //        }
+    //    }
 }
 
 struct ContentView_Previews: PreviewProvider {
