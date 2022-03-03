b8ce2bf2fa14d63b4a277a7dcd7d66fd1fa0ba5a
citixenken <kmuyesu@gmail.com>
2022-03-03 22:53:13 +0300

    adding context menu to an image

--- HotProspects/MeView.swift
+++ HotProspects/MeView.swift
@@ -12,6 +12,7 @@ struct MeView: View {
     
     @State private var name = "Anon"
     @State private var emailAddress = "anon@anonymous.io"
+    @State private var qrCode = UIImage()
     
     let context = CIContext()
     let filter = CIFilter.qrCodeGenerator()
@@ -23,17 +24,30 @@ struct MeView: View {
                     .textContentType(.name)
                     .font(.title)
                 
-                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                Image(uiImage: qrCode)
+                    .interpolation(.none) //neat!
                     .resizable()
-                    .interpolation(.none) //neat
                     .scaledToFit()
                     .frame(width: 200, height: 200)
+                    .contextMenu {
+                        Button {
+                            //save my QR code
+                            //let image = generateQRCode(from: "\(name)\n\(emailAddress)")
+                            let imageSaver = ImageSaver()
+                            imageSaver.writeToPhotoAlbum(image: qrCode)
+                        } label: {
+                            Label("Save to Photos", systemImage: "square.and.arrow.down")
+                        }
+                    }
                 
                 TextField("Your email address", text: $emailAddress)
                     .textContentType(.emailAddress)
                     .font(.title)
             }
             .navigationTitle("Your code")
+            .onAppear(perform: updateCode)
+            .onChange(of: name) { _ in updateCode() }
+            .onChange(of: emailAddress) { _ in updateCode()}
         }
     }
     
@@ -43,9 +57,15 @@ struct MeView: View {
         if let outputImage = filter.outputImage {
             if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
                 return UIImage(cgImage: cgimg)
+//                qrCode = UIImage(cgImage: cgimg)
+//                return qrCode
             }
         }
         return UIImage(systemName: "xmark.circle") ?? UIImage()
+    }
+    
+    func updateCode() {
+        qrCode = generateQRCode(from: "\(name)\n\(emailAddress)")
     }
 }
 
